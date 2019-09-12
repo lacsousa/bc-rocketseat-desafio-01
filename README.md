e4e5f00424c0c01ef338e3deb27ec37b34dfe4ff
Luciano Cordeiro <lacsousa@yahoo.com.br>
2019-09-11 23:25:17 -0300

    Desafio finalizado

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # bc-rocketseat-desafio-01
 Desafio 01
+
+Conceitos do NodeJS
+Aplicação do zero utilizando Express.
+
+Essa aplicação será utilizada para armazenar projetos e suas tarefas.
+
+Rotas
+POST /projects: A rota deve receber id e title dentro corpo de cadastrar um novo projeto dentro de um array no seguinte formato: { id: "1", title: 'Novo projeto', tasks: [] }; 
+
+GET /projects: Rota que lista todos projetos e suas tarefas;
+
+PUT /projects/:id: A rota deve alterar apenas o título do projeto com o id presente nos parâmetros da rota;
+
+DELETE /projects/:id: A rota deve deletar o projeto com o id presente nos parâmetros da rota;
+
+POST /projects/:id/tasks: A rota deve receber um campo title e armazenar uma nova tarefa no array de tarefas de um projeto específico escolhido através do id presente nos parâmetros da rota;
+
+Exemplo
+Se eu chamar a rota POST /projects repassando { id: 1, title: 'Novo projeto' } e a rota POST /projects/1/tasks com { title: 'Nova tarefa' }, meu array de projetos deve ficar assim:
+
+[
+  {
+    id: "1",
+    title: 'Novo projeto',
+    tasks: ['Nova tarefa']
+  }
+]
+
+Middlewares
+1 - Middleware será utilizado em todas rotas que recebem o ID do projeto nos parâmetros da URL que verifica se o projeto com aquele ID existe. Se não existir retorne um erro, caso contrário permita a requisição continuar normalmente;
+
+2 - Middleware global chamado em todas requisições que imprime (console.log) uma contagem de quantas requisições foram feitas na aplicação até então;
--- index.js
+++ index.js
@@ -4,15 +4,93 @@ const server = express();
 
 server.use(express.json());
 
+let quantReq = 0;
 const projetos = [
-    { id: "1", title: 'Novo projeto', tasks: ['Tarefa1','Tarefa2' ] },
-    { id: "2", title: 'Projeto A ', tasks: ['Tarefa3', 'Tarefa3', 'Tarefa4'] },
-    { id: "3", title: 'Projeto B', tasks: ['Tarefa5'] }
-
+  { id: '1', title: 'Novo projeto', tasks: ['Tarefa1', 'Tarefa2'] },
+  { id: '2', title: 'Projeto A ', tasks: ['Tarefa3', 'Tarefa3', 'Tarefa4'] },
+  { id: '3', title: 'Projeto B', tasks: ['Tarefa5'] }
 ];
 
+
+// Middleware Global
+function quantRequests(req, res, next) {
+    
+    quantReq ++;
+    console.log(`A quantidade de requisições foi de : ${quantReq}`);
+
+    next();
+}
+
+server.use(quantRequests);
+
+//middleware Específico
+function checkProjectInArray(req, res, next){
+    const projeto = projetos[req.params.index];
+
+    if(!projeto) {
+        return res.status(400).json({ error : "Projeto não existe!"});
+    }
+
+    req.projeto = projeto;
+
+    return next();
+}
+
+
 server.get('/projects', (req, res) => {
+  return res.json(projetos);
+});
+
+
+server.get('/project/:index', checkProjectInArray, (req, res) => {
+
+  // Após o Middleware alterar o conteúdo do req, não precisaremos mais da linha abaixo  
+  // const { index } = req.params;
+
+    // Além do retorno tb ser alterado
+//   return res.json(projetos[index]);
+  return res.json(req.projeto);
+});
+
+
+server.post('/projects', (req, res) => {
+  const { id } = req.body;
+  const { title } = req.body;
+
+  const novoReg = { "id": id, "title": title, tasks: [] };
+
+  projetos.push(novoReg);
+
+  return res.json(projetos);
+});
+
+
+server.put('/project/:index', checkProjectInArray, (req, res) => {
+  const { index } = req.params;
+  const { title } = req.body;
+
+  projetos[index].title = title;
+
+  return res.json(projetos);
+});
+
+server.post('/project/:index/tasks', checkProjectInArray, (req, res) => {
+    const { index } = req.params;
+    const { title } = req.body;
+
+    projetos[index].tasks.push(title);
+
     return res.json(projetos);
 });
+
+server.delete('/project/:id', (req,res) => {
+    const { id } = req.params;
+
+    projetos.splice(id, 1);
+
+    return res.send();
+
+});
+
 
 server.listen(3001);
